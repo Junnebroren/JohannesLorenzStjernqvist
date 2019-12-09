2a20d05695623a04a795591ba0f483023d6bd6c4
Junnebroren <lorenz.johannes@hotmail.com>
2019-12-09 10:21:52 +0100

    added sub method to the math class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -1,7 +1,12 @@
 package com.company;
 
 public class Math {
-    public int add (int first,int second){
-        return first+second;
+    public int add(int first, int second) {
+        return first + second;
     }
 }
+    public class sub {
+        public int sub(int first, int second) {
+            return first+second;
+        }
+    }
